9f823baf5993a7b96637b64eadef189b1dc0b627
Rodrigo Moraes <126883533+rodrigo088123344@users.noreply.github.com>
2023-11-06 11:27:39 -0400

    Add files via upload

---
+++ cad.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<header>
+    <h1>Resultado do processamento</h1>
+</header>
+<main>
+    <?php
+    $n = $_GET ["nome"];
+    $s = $_GET ["sobrenome"];
+    echo "<p> É um prazer te conhecer,<strong> $n $s </strong>! Este é meu lugar site;"
+    ?>
+    <p>
+        <a href="javascript:history.go(-1)">Voltar para pagina anterior</a>
+    </p>
+</main>
+
+</body>
+</html>
